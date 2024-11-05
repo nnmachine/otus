8c17e481439310fe3ef1d4cb9925d37809cbd7e7
novveak <nomickll@gmail.com>
2024-11-05 07:39:19 +0300

    edit 12.py

--- 1/12.py
+++ 1/12.py
@@ -5,10 +5,10 @@ from math import sqrt
 def solution(side: int, r: int):
     for y in range(side):
         for x in range(side):
-            if y > r:
-                print(". ", end="")
-            else:
-                print("# " if x <= sqrt(r * r - y * y) else ". ", end="")
+            print(
+                "# " if y <= r and x <= sqrt(r * r - y * y) else ". ",
+                end="",
+            )
 
         print()
 
