17506fa25254d49176d49373057d7db25665b785
novveak <nomickll@gmail.com>
2024-11-05 07:56:53 +0300

    simplify 12.py

--- 1/12.py
+++ 1/12.py
@@ -6,7 +6,7 @@ def solution(side: int, r: int):
     for y in range(side):
         for x in range(side):
             print(
-                "# " if y <= r and x <= sqrt(r * r - y * y) else ". ",
+                "# " if x**2 + y**2 <= r**2 else ". ",
                 end="",
             )
 
